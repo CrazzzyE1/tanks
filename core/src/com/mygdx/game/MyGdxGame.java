c59106aa710d7e3237565b92a42c9fd1795b23c5
CrazzzyE1 <litvakivan@gmail.com>
2019-08-22 16:44:25 +0300

    Start Second Day's version

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+// to Second version - Note REALISE!!!
 public class MyGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Tank tank;
