bcf6c8a2ee41c4492cde616002afa9a1f4467200
CrazzzyE1 <litvakivan@gmail.com>
2019-08-23 09:54:22 +0300

    Change Bullet's x-y to Vector 2

--- core/src/com/mygdx/game/Bullet.java
+++ core/src/com/mygdx/game/Bullet.java
@@ -2,13 +2,12 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Bullet {
     private Texture texture;
-    private float x;
-    private float y;
-    private float vx;
-    private float vy;
+    private Vector2 position;
+    private Vector2 velocity;
     private boolean active;
 
     public boolean isActive() {
@@ -17,15 +16,13 @@ public class Bullet {
 
     public Bullet() {
         texture = new Texture("projectile.png");
-        this.x = 0.0f;
-        this.y = 0.0f;
-        this.vx = 0.0f;
-        this.vy = 0.0f;
+        this.position = new Vector2();
+        this.velocity = new Vector2();
         this.active = false;
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, x - 8, y - 8);
+        batch.draw(texture, position.x - 8, position.y - 8);
     }
 
     public void deactivate () {
@@ -34,17 +31,13 @@ public class Bullet {
 
     public void activate (float x, float y, float vx, float vy) {
         this.active = true;
-        this.x = x;
-        this.y = y;
-        this.vx = vx;
-        this.vy = vy;
-
+        this.position.set(x, y);
+        this.velocity.set(vx, vy);
     }
 
     public void update (float dt) {
-        x += vx * dt;
-        y += vy * dt;
-        if (x < 0.0f || x > 1280.0f || y < 0.0f || y > 720.0f) {
+        position.mulAdd(velocity, dt);
+        if (position.x < 0.0f || position.x > 1280.0f || position.y < 0.0f || position.y > 720.0f) {
             active = false;
         }
     }
