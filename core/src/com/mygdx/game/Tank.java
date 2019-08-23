59cb391817d93009b0f061468660267cb6dda618
CrazzzyE1 <litvakivan@gmail.com>
2019-08-23 09:44:47 +0300

    Change x-y to Vector2

--- core/src/com/mygdx/game/Tank.java
+++ core/src/com/mygdx/game/Tank.java
@@ -4,25 +4,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Tank {
     private MyGdxGame game;
     private Texture texture;
-    private  float x;
-    private float y;
+    private Vector2 position;
     private float speed;
     private float angle;
 
     public Tank(MyGdxGame game) {
         this.game = game;
         this.texture = new Texture("player_tank_base.png");
-        this.x = 0.0f;
-        this.y = 0.0f;
+        this.position = new Vector2(100.0f, 100.0f);
         this.speed = 100.0f;
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, x - 20, y - 20, 20, 20 , 40,40,1,1, angle, 0, 0, 40,40,false,false);
+        batch.draw(texture, position.x - 20, position.y - 20, 20, 20 , 40,40,1,1, angle, 0, 0, 40,40,false,false);
 
     }
 
@@ -35,22 +34,22 @@ public class Tank {
 
     public void checkMovement(float dt) {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            x -= speed * dt;
+            position.x -= speed * dt;
             angle = 180.0f;
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            x += speed * dt;
+            position.x += speed * dt;
             angle = 0.0f;
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)){
-            y += speed * dt;
+            position.y += speed * dt;
             angle = 90.0f;
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            y -= speed * dt;
+            position.y -= speed * dt;
             angle = 270.0f;
             return;
         }
@@ -59,7 +58,7 @@ public class Tank {
     public void fire() {
         if (!game.getBullet().isActive()) {
             float angleRad = (float)Math.toRadians((angle));
-            game.getBullet().activate(x, y, 320.0f*(float)Math.cos(angleRad), 320.0f*(float)Math.sin(angleRad));
+            game.getBullet().activate(position.x, position.y, 320.0f*(float)Math.cos(angleRad), 320.0f*(float)Math.sin(angleRad));
         }
     }
 
